d484e3d9c9af20f4a01d6ce2cc34110cceb907c2
We bare bears <43983703+chavilaw@users.noreply.github.com>
2021-12-13 14:35:23 +0200

    update

--- activity.js
+++ activity.js
@@ -2,3 +2,26 @@
 
 let element = document.querySelector("p[color='black']");
 console.log(element); // logs null
+
+
+
+var ctx = document.getElementById("activity");
+
+var stars = [135850, 52122, 148825, 16939, 9763];
+var frameworks = ["React", "Angular", "Vue", "Hyperapp", "Omi"];
+
+var activity = new Chart(ctx, {
+    type: "line",
+    data: {
+        labels: frameworks,
+        datasets: [
+            {
+                label: "Github Stars",
+                data: stars,
+                backgroundColor: "rgba(255, 99, 132, 0.2)",
+                borderColor: "rgba(255, 99, 132, 1)",
+                borderWidth: 1
+            }
+        ]
+    }
+});
