60cb00119ec00c1baa459e67ade8cc40c7f3ecdf
Arnaud Avocat Gros <a.avocatg@gmail.com>
2021-12-16 16:13:44 +0200

    create session storage on login

--- login.js
+++ login.js
@@ -25,14 +25,37 @@ loginButton.addEventListener("click", (e) => {
     .then(result => {
         if(result !== "wrong input"){
             console.log(result);
-            
+            sessionStorage.setItem('id', result.id);
+            sessionStorage.setItem('name', result.name);
+            sessionStorage.setItem('isLogged', 'true');
+            window.location = "main-page.html";
+        }
+        else{
+            loginErrorMsg.style.opacity = 1;
         }
     })
     .catch(error => console.log('error', error));
-    if (username === "user" && password === "otto") {
-        window.location = "main-page.html";
-        return true;
-    } else {
-        loginErrorMsg.style.opacity = 1;
+    
+});
+
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+
+  function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
     }
-})
+    return "";
+  }
