2d7bb9d9338883ec557afd8d7e3a0a1414fdc481
Arnaud Avocat Gros <a.avocatg@gmail.com>
2021-12-17 10:23:57 +0200

    Revert "fetch api for heart-rate"
    This reverts commit bdd0c5a9a45d1a5aef207f2cb8c448a5f6ae2082.

--- heart-rate.js
+++ heart-rate.js
@@ -1,38 +1,10 @@
-/*$.getJSON('heart-rate.json', function(data) {
+$.getJSON('heart-rate.json', function(data) {
     console.log(data); //see your data ( works in Chrome / FF with firebug)
     console.log(data["date"]["value"]) //should output 1
 });
-*/
-
-var myHeaders = new Headers();
-myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
-
-var bodyRequest = new URLSearchParams();
-bodyRequest.append("id", 1);
-
-var requestOptions = {
-    method: 'POST',
-    headers: myHeaders,
-    body: bodyRequest
-};
-
-fetch("http://127.0.0.1:1337/heartrate", requestOptions)
-.then(response => response.json())
-.then(result => {
-    let array = [];
-    let values = [];
-    result.forEach(element => {
-        let splited = element.date.split('T');
-        let splited2 = splited[1].split(':');
-        let final = splited2[0].toString() + splited2[1].toString();
-        console.log(final);
-        array.push(final);
-
-        console.log(element.value);
-        values.push(element.value);
 
 
-    });
-    console.log(array);
-    console.log(values);
-});
+
+fetch("heart-rate.json")
+    .then(response => response.json())
+    .then(json => console.log(json));
